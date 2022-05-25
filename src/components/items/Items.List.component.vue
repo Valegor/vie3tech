47676815137789a77166cdf3b3aed6040113ca94
Valegor <medifit2000@yandex.ru>
2022-05-25 20:05:26 +0300

    add click

--- src/components/items/Items.List.component.vue
+++ src/components/items/Items.List.component.vue
@@ -2,18 +2,27 @@
     <div>
         <h3>Items:</h3>
         <ul>
-            <li v-for="item in items" :key="item.id">{{ item.name }}</li>
+            <li v-for="item in items" :key="item.id" @click="onItemSelect(item)">
+                {{ item.name }}
+            </li>
         </ul>
     </div>
 </template>
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
-import { ItemInterface } from '@/models/items/Item.interface'
+import { ItemInterface } from "@/models/items/Item.interface";
 export default defineComponent({
     props: {
         items: {
-            type: Array as PropType<ItemInterface[]>
+            type: Array as PropType<ItemInterface[]>,
         },
+    },
+    setup() {
+        const onItemSelect = (item: ItemInterface) => {
+            item.selected = !item.selected;
+            console.log("onItemSelect", item.id, item.selected);
+        };
+        return { onItemSelect };
     },
 });
 </script>
