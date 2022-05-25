4e05d3aabe8ff158098368e0d1a20ef37f3332bb
Valegor <medifit2000@yandex.ru>
2022-05-25 20:49:12 +0300

    add children component

--- src/components/items/Items.List.component.vue
+++ src/components/items/Items.List.component.vue
@@ -1,28 +1,48 @@
 <template>
-    <div>
-        <h3>Items:</h3>
-        <ul>
-            <li v-for="item in items" :key="item.id" @click="onItemSelect(item)">
-                {{ item.name }}
-            </li>
-        </ul>
-    </div>
+  <div>
+    <h3>Items:</h3>
+    <ul>
+      <ItemComponent
+        v-for="item in items"
+        :key="item.id"
+        :model="item"
+        @select="onItemSelect"
+      />
+    </ul>
+  </div>
 </template>
+
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
 import { ItemInterface } from "@/models/items/Item.interface";
+import ItemComponent from "./children/Item.component.vue";
+
 export default defineComponent({
-    props: {
-        items: {
-            type: Array as PropType<ItemInterface[]>,
-        },
+  components: {
+    ItemComponent,
+  },
+  props: {
+    items: {
+      type: Array as PropType<ItemInterface[]>,
     },
-    setup() {
-        const onItemSelect = (item: ItemInterface) => {
-            item.selected = !item.selected;
-            console.log("onItemSelect", item.id, item.selected);
-        };
-        return { onItemSelect };
-    },
+  },
+  setup() {
+    const onItemSelect = (item: ItemInterface) => {
+      item.selected = !item.selected;
+      console.log("onItemSelect", item.id, item.selected);
+    };
+    return { onItemSelect };
+  },
 });
 </script>
+
+<style lang="scss">
+ul {
+  list-style-type: none;
+  margin-block-start: 0;
+  margin-block-end: 0;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  padding-inline-start: 0px;
+}
+</style>
---
+++ tests/unit/components/items/Item.component.spec.ts
@@ -0,0 +1,59 @@
+import { expect } from "chai";
+import { shallowMount } from "@vue/test-utils";
+import ItemComponent from "@/components/items/children/Item.component.vue";
+import { ItemInterface } from "@/models/items/Item.interface";
+
+describe("Item.component.vue", () => {
+    it("renders an Item correctly", () => {
+        const model: ItemInterface = {
+            id: 1,
+            name: "Unit test item 1",
+            selected: false,
+        };
+        const wrapper = shallowMount(ItemComponent, {
+            props: {
+                model: model,
+            },
+        });
+        // простая проверка, что в выводе компонента есть
+        // строка 'Unit test item 1', но это не очень правильно expect(wrapper.text()).to.include('Unit test item 1')
+        // это более правильный подход: сначала находим нужный // элемент (div с классом `name`), а затем проверяем, // содержится ли в нём нужный текст
+        let domEl = wrapper.find("div.name");
+        expect(domEl.text()).to.equal("Unit test item 1");
+    });
+});
+
+it("has expected css class when selected is false", () => {
+    const model: ItemInterface = {
+        id: 2,
+        name: "Unit test item 2",
+        selected: false,
+    };
+    const wrapper = shallowMount(ItemComponent, {
+        props: {
+            model: model,
+        },
+    });
+    // проверка списка CSS-классов в компоненте
+    const classes = wrapper.classes();
+    expect(classes).to.be.an("array").that.includes("item");
+    expect(classes).to.be.an("array").that.does.not.includes("selected");
+});
+
+it("has selected css class when selected is true", () => {
+    const model: ItemInterface = {
+        id: 3,
+        name: "Unit test item 3",
+        selected: true /* setting selected = true here */,
+    };
+    const wrapper = shallowMount(ItemComponent, {
+        props: {
+            model: model,
+        },
+    });
+    // check component css classes list
+    const classes = wrapper.classes();
+    expect(classes).to.be.an("array").that.includes("item");
+
+    expect(classes).to.be.an("array").that.includes("selected");
+});
